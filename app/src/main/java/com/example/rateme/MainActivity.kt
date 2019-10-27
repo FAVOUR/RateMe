5a089d3321c70c3a17c3fbc85788c14eaa83be0b
FAVOUR <truelyfavoured@gmail.com>
2019-10-27 22:34:10 +0100

    Concluded userevent with viewmodel exempting the progress  bar

--- app/src/main/java/com/example/rateme/MainActivity.kt
+++ app/src/main/java/com/example/rateme/MainActivity.kt
@@ -3,16 +3,25 @@ package com.example.rateme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.example.rateme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       val datatoBind: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        datatoBind.lastName ="Olije"
-        datatoBind.setFirstName("Favour")
+        val userViewModel:UserViewModel=ViewModelProviders.of(this)[UserViewModel::class.java]
+       val datatoBind: ActivityMainBinding = DataBindingUtil. setContentView(this,R.layout.activity_main)
+
+//        datatoBind.lastName ="Olije"
+//        datatoBind.setFirstName("Favour")
+
+        datatoBind.lifecycleOwner = this
+
+        datatoBind.viewmodel=userViewModel
 
     }
 }
